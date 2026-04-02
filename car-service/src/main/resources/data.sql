b075ebcf91eaebbe0dc755065825aba78e6f21a5
DarraghAI835 <A00314039@Student.tus.ie>
2026-04-02 13:16:01 +0100

    dd

--- car-service/src/main/resources/data.sql
+++ car-service/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into cars(MAKE,MODEL,AGE,COLOUR,GARAGE_ID)values('VW','Golf',2022,'Silver',1)
-insert into cars(MAKE,MODEL,AGE,COLOUR,GARAGE_ID)values('VW','Polo',2002,'Gold',2)
-insert into cars(MAKE,MODEL,AGE,COLOUR,GARAGE_ID)values('Ford','Fiesta',2012,'Gray',3)
+insert into cars(MAKE,MODEL,AGE,COLOUR,DEALERSHIP_ID)values('VW','Golf',2022,'Silver',1)
+insert into cars(MAKE,MODEL,AGE,COLOUR,DEALERSHIP_ID)values('VW','Polo',2002,'Gold',2)
+insert into cars(MAKE,MODEL,AGE,COLOUR,DEALERSHIP_ID)values('Ford','Fiesta',2012,'Gray',3)
